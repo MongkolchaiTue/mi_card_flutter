6b1081ee7198385094c400ddd07a731c451f30a4
MTue2023 <{ID}+{username}@users.noreply.github.com>
2024-01-20 23:46:00 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	macos/Flutter/ephemeral/Flutter-Generated.xcconfig
    #	macos/Flutter/ephemeral/FlutterInputs.xcfilelist
    #	macos/Flutter/ephemeral/FlutterOutputs.xcfilelist
    #	macos/Flutter/ephemeral/flutter_export_environment.sh

--- lib/demo_async.dart
+++ lib/demo_async.dart
@@ -1,24 +1,40 @@
-Future<void> printOrderMessage() async {
-  print('Awaiting user order...');
-  var order = await fetchUserOrder();
-  print('Your order is: $order');
 
+Future<void> printOrderMessage() async {
+  try {
+    print('Awaiting user order...');
+    var order = await fetchUserOrder();
+    print('Your order is: $order');
+  } catch (err) {
+    print('Order -> Caught error: $err');
+    throw 'Cannot locate Order';
+  }
 }
 
 Future<String> fetchUserOrder() {
-  //Imagine that this function is more comples and slow.
-  return Future.delayed(const Duration(seconds: 5), () => 'Lerge Latte');
-
-
+  //Imagine that this function is more complex and slow.
+  var str = Future.delayed(
+      //const Duration(seconds: 5), () => 'Large Latte');
+      const Duration(seconds: 5),
+      () => throw 'Cannot locate user order');
+  return str;
 }
 
 void main() async {
-  countSeconds(4);
-  await printOrderMessage();
+  try {
+    countSeconds(4);
+    await printOrderMessage();
+  } catch (err) {
+    print('main -> Caught error: $err');
+  }
 }
 
-void countSeconds(int i) {
-  for (var x = 1; x <= i; x++) {
+int countSeconds(int i) {
+  // Stream<List<int>> stream = File('demo_async.dart').openRead();
+  // stream.transform(utf8.decoder).forEach(print);
+
+  for (int x = 1; x <= i; x++) {
     Future.delayed(Duration(seconds: 1),() => print(x));
   }
+
+  return i++;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 backgroundColor: Colors.red,
@@ -40,48 +40,52 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                color: Colors.white,
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.home,
-                      size: 40.0,
-                      color: Colors.teal.shade600,
-                    ),
-                    Text(
-                      'Tel.+66 123 456 789',
-                      style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                        letterSpacing: 2.5,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child:  Divider(
+                  color: Colors.teal.shade100,
                 ),
               ),
               Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    size: 40.0,
+                    color: Colors.teal.shade600,
+                  ),
+                  title: Text(
+                    'Tel.+66 123 456 789',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                        'angela at email.com',
-                      style: TextStyle(
+                  ),
+                ),
+              ),
+
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'angela@email.com',
+                    style: TextStyle(
                         fontSize: 20.0,
                         color: Colors.teal.shade900,
                         fontFamily: 'Pacifico'
-                      ),
                     ),
-                  ],
-                )
+                  ),
+                ),
               ),
             ],
           ),
